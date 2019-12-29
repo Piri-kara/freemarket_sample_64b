98c382f081e7cfd4183a3031b8631bebb62db8ec
Piri-kara <hpt_q@yahoo.co.jp>
2019-12-30 03:58:08 +0900

    Capistrano編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.11.2"
 
 set :application, "freemarket_sample_64b"
-set :repo_url, "git@github.com:Ryu1234-tech/freemarket_sample_64b.git"
+set :repo_url, "git@github.com:Piri-kara/freemarket_sample_64b.git"
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -12,7 +12,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/mercari64b.pem'] 
+                  keys: ['~/.ssh/freemarket_sample_64b.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
